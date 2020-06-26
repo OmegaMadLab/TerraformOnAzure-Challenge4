49d3f9d44eef650563ce5fe05b82f4962f0c896a
Marco Obinu <locurtola@tin.it>
2020-06-26 08:16:46 +0200

    Final version

--- README.md
+++ README.md
@@ -5,12 +5,17 @@ The fourth challenge of the [Terraform On Azure](https://github.com/Terraform-On
 We need to create an AKS cluster via Terraform. Then, we have to prepair a container for our sample app, publish it to an Azure Container Registry and deploy it to the AKS cluster via Helm.
 You can read more about the challenge [here](https://github.com/Terraform-On-Azure-Workshop/terraform-azure-hashiconf2020/blob/main/challenges/challenge4/Readme.md).
 
+I used Terraform to deploy a resource group with an AKS and an ACR.
 
+Locally, I used **init.sh** to:
+- install kubectl
+- install helm
+- install docker
 
-I used **init.sh** to:
-- create the storage account and show its keys
-- create a service principal and show its credentials
+Then, I used **build-and-deploy.sh** inside the folder where I cloned the repo to:
+- build the image and tag it
+- push the image to ACR
+- connect to AKS and federate it with ACR
+- deploy the app with Helm
 
-Then, I saved them as environment variables in GitHub Actions, and setup a YAML pipeline to deploy the TF infrastructure.
-
-You can find init.sh in the */setup folder*, while the YAML pipeline is in *.github/workflows/*.  
+I executed all the steps above on Ubuntu 18.04 WSL2.
--- build-deploy-image.sh
+++ build-deploy-image.sh
@@ -13,13 +13,19 @@ docker rm --force tfonazure
 az acr login --name omegamadlabtfonaz4acr
 
 # Tag the image
-docker tag tfonazure-challenge4:latest omegamadlabtfonaz4acr.azurecr.io/tfonazure/tfonazure-challenge4
+docker tag tfonazure-challenge4 omegamadlabtfonaz4acr.azurecr.io/tfonazure:v1
 
 # Push the image to ACR
-docker push omegamadlabtfonaz4acr.azurecr.io/tfonazure/tfonazure-challenge4
+docker push omegamadlabtfonaz4acr.azurecr.io/tfonazure:v1
 
 # Show the image in ACR
 az acr repository list --name omegamadlabtfonaz4acr
+
+ACRLOGIN=$(az acr show --name "omegamadlabtfonaz4acr" \
+            --resource-group "tfonazure-challenge4-RG" \
+            --query "loginServer" -o tsv)
+
+docker images $ACRLOGIN/tfonazure
 
 ## Deploy with Help
 cd ../../Deploy/helm/
@@ -31,16 +37,17 @@ echo $AKSHOST
 # Connect to AKS
 az aks get-credentials --resource-group "tfonazure-challenge4-RG" --name $AKSHOST
 
-helm upgrade --install "tfonazure-challenge4" \
-    -f "gvalues.yaml" \
-    -f "values.b2c.yaml" \
+# Integrate AKS with ACR
+az aks update --name "omegamadlab-tfonaz-4-aks" \
+    --resource-group "tfonazure-challenge4-RG" \
+    --attach-acr "omegamadlabtfonaz4acr"
+
+# Deploy Helm chart
+helm upgrade --install tfonazure \
+    ./web \
+    -f ./gvalues.yaml \
+    -f ./values.b2c.yaml \
     --set ingress.hosts={$AKSHOST} \
     --set ingress.protocol=http \
-    --set image.repository=omegamadlabtfonaz4acr.azurecr.io/tfonazure/tfonazure-challenge4 \
-    --set image.tag=latest \
-    --verify \
-    --wait \
-    ./web 
-#helm upgrade --install tailwindtraders-web ./Deploy/helm/web -f ./Deploy/helm/gvalues.yaml -f ./Deploy/helm/values.b2c.yaml  --set ingress.hosts={challenge4-aks1}  --set ingress.protocol=https --set ingress.tls[0].hosts={challenge4-aks1}  --set image.repository=challenge4acrbrkuhlma.azurecr.io --set image.tag=gh-${{ github.sha }}
-
-helm uninstall "tfonazure-challenge4"
+    --set image.repository=$ACRLOGIN/tfonazure \
+    --set image.tag=v1 
